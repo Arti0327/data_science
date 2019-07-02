054cb1a205104a25e9308a4d42a21f5757a7b9d4
arti0327 <aarti7697@gmail.com>
2019-07-02 10:46:09 +0530

    arti

---
+++ strip.py
@@ -0,0 +1,30 @@
+a="hello world"
+print (a)
+a1=a+" surbhi"
+print(a1)
+print("indexing")
+b="hello world"
+print(b[4])
+print(b[8])
+print(b[9])
+
+print("slicing")
+c="arti mishra"
+print(c[2:8])
+print(c[:7])
+print(c[7:])
+print(c[-10])
+print"replace"
+txt= "pratiksha mishra"
+e=txt.replace("pratiksha","nitin")
+print(e)
+print"strip"
+f=" raksha vishwkrma "
+print(f.rstrip())
+t="hello world"
+t1=t.strip()
+print(t.strip("h"))
+g="hHaHaHahhh"
+print(g.strip("h"))
+print(g.lstrip("h"))
+
